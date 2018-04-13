9564e017e207063d9ddb3033ef4ea33332bf76cf
forbywrytz <38335708+forbywrytz@users.noreply.github.com>
2018-04-14 00:46:00 +0100

    Add files via upload

---
+++ HNG/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>HNG INTERNSHIP 4</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <section class="home">
+      <div class="background">
+        <div class="text">
+          <?php
+           echo "<h1>" .date("h:i") ."</h1>";
+          ?>
+        </div>
+      </div>
+    </section>
+  </body>
+</html>
